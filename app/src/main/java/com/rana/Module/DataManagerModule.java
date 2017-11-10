8d8750b4fde73c7a4d6bc7bcfc4daaa47ce2364a
Rana Prathap <rana.droid07@gmail.com>
2017-11-09 20:28:56 -0600

    DataManagerModuler add

---
+++ app/src/main/java/com/rana/Module/DataManagerModule.java
@@ -0,0 +1,11 @@
+package com.rana.Module;
+
+/**
+ * Created by Rana Prathap on 11/9/2017.
+ */
+
+public class DataManagerModule {
+
+
+    // this the manages the data and shared preferences...
+}
